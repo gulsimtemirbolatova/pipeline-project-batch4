83815af9a957d189543b891363e17a85738bd379
ethantechtorial <etan@techtorialacademy.com>
2023-10-20 01:45:52 +0000

    3rd update

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java this is 2nd update");
+        System.out.println("Hello from Java this is 3rd update");
     }
 }
